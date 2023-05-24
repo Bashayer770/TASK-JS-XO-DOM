b73d3f87cd3aac83f954d8896b3832d0595c1c03
bashayer770 <beshow95@gmail.com>
2023-05-24 18:11:41 +0300

    X&O

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function fillButton(index, text) {
 }
 // pre-made a function. You can use this function to present an alert to say someone wins
 function winningAlert(winner) {
-  if(confirm(`Horraaay, ${winner} wins!`)){
-     // The code here will be exectued if you press on OK button that will pop on the window 
+  if (confirm(`Horraaay, ${winner} wins!`)) {
+    // The code here will be exectued if you press on OK button that will pop on the window
   }
 }
 
@@ -27,7 +27,50 @@ fillButton(9, "O");
 function clickButton(index) {
   console.log(`Button number ${index} is clicked`);
   // Your main code here.
+
+  let turn = 0;
+  const p1 = "X";
+  const p2 = "O";
+
+  let currentPlayer = p1;
+  const button = document.getElementById(index).innerHTML;
+  if (button !== "") {
+    console.log("Button already filled.");
+    return;
+  }
+
+  if (turn === 9) {
+    alert("It's a draw!");
+    return;
+  }
+
+  currentPlayer = currentPlayer === p1 ? p2 : p1;
 }
+
+fillButton(index, currentPlayer);
+
+function checkWinner(player) {
+  const winners = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [3, 5, 7],
+    [1, 5, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+  ];
+  return winners.some((x) => {
+    const [a, b, c] = x;
+    const buttonA = document.getElementById(a).innerHTML;
+    const buttonB = document.getElementById(b).innerHTML;
+    const buttonC = document.getElementById(c).innerHTML;
+
+    return buttonA === player && buttonB === player && buttonC === player;
+  });
+}
+
+function restartGame() {}
 
 /**
  * (Optional) It's always a good idea to make a function for every single purpose.
